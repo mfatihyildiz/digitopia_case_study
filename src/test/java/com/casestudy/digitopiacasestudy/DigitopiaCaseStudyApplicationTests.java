9ee5d412f051a399a0c44e57bf663ac8dae6faf0
Fatih Yıldız <charvisfk@gmail.com>
2025-10-04 13:04:35 +0300

    Digitopia Case Study setup

---
+++ src/test/java/com/casestudy/digitopiacasestudy/DigitopiaCaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casestudy.digitopiacasestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitopiaCaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
