7184a779492d2387903bcd9a4cc04c2b6c062e24
Fatih Yıldız <charvisfk@gmail.com>
2025-10-05 13:45:30 +0300

    OpenApi refactoring

--- src/main/java/com/casestudy/digitopiacasestudy/config/OpenApiConfig.java
+++ src/main/java/com/casestudy/digitopiacasestudy/config/OpenApiConfig.java
@@ -4,8 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -20,6 +23,9 @@ public class OpenApiConfig {
                         .contact(new Contact()
                                 .name("Mehmet Fatih Yıldız")
                                 .email("example@mail.com"))
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080")
+                                .description("Local Development Server")));
     }
 }
--- src/main/java/com/casestudy/digitopiacasestudy/controller/HealthController.java
+++ src/main/java/com/casestudy/digitopiacasestudy/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/healtz")
+    @GetMapping("/health")
     public String healthCheck() {
         return "OK";
     }
