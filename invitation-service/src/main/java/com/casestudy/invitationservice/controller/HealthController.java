40dcb939cb83bbdf0e61d53db01fcfbd81d474db
Fatih Yıldız <charvisfk@gmail.com>
2025-10-10 10:29:16 +0300

    Health check methods

---
+++ invitation-service/src/main/java/com/casestudy/invitationservice/controller/HealthController.java
@@ -0,0 +1,22 @@
+package com.casestudy.invitationservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/healthz")
+    public ResponseEntity<Map<String, Object>> healthCheck() {
+        Map<String, Object> status = new HashMap<>();
+        status.put("status", "UP");
+        status.put("timestamp", LocalDateTime.now().toString());
+        status.put("service", "OK");
+        return ResponseEntity.ok(status);
+    }
+}
